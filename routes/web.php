05874134842c1ce1194adc2e56362cf2e226f7a7
ARVAZAKI <arvazf09@gmail.com>
2024-09-18 08:48:23 +0700

    installing breeze livewire

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+Route::view('/coba', 'tes')
+    ->name('tes');
+
+require __DIR__.'/auth.php';
