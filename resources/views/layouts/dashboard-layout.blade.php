fb690a167e74dfa2c78dc9d228f31b24a13170c3
ARVAZAKI <arvazf09@gmail.com>
2024-09-18 18:09:02 +0700

    update livewire welcome page

--- resources/views/layouts/dashboard-layout.blade.php
+++ resources/views/layouts/dashboard-layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>EduSecure | @yield('title')</title>
     @vite('resources/css/app.css')
 </head>
 
@@ -41,7 +41,7 @@
                     </div>
                 </div>
             </div>
-            <div>testing</div>
+            <div>@yield('content')</div>
         </div>
     </div>
 
