9dd490afe53920b8a1c66a1a30d2888b62702555
Dio Ramadhan <dioramadhan9@gmail.com>
2024-09-20 13:45:28 +0700

    tes2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+Route::view('/test', 'layouts.dashboard-layout');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+Route::view('/coba', 'tes')
+    ->name('tes');
+
+require __DIR__ . '/auth.php';
